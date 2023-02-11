2943b2d859272656fe7d0daee09bc34445d6b31b
NathanFelipe99 <nathanfguerlando@gmail.com>
2023-02-11 20:33:18 -0300

    feat: consuming GoogleSheets API

--- src/application/services/googleSheetsService/googleSheets.service.js
+++ src/application/services/googleSheetsService/googleSheets.service.js
@@ -1,21 +1,46 @@
-const { authenticate } = require("@google-cloud/local-auth");
 const { google } = require("googleapis");
-const path = require("path");
+const { GoogleSpreadsheet } = require("google-spreadsheet");
+const credentials = require("./../../../../credentials.json");
 
 class GoogleSheetsService {
-    #SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
-    #CREDENTIAL_PATH = path.join(process.cwd(), "credentials.json");
-    
+    #SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
+
     async execute(sheetID) {
+        const sheetData = await this.#getData(sheetID);
 
+        return sheetData;
     }
 
-    async #authorize() {
+    async #getData(sheetID) {
+        const doc = new GoogleSpreadsheet(sheetID);
+        await doc.useServiceAccountAuth(credentials);
+        await doc.loadInfo();
         
-    }
+        const sheet = doc.sheetsByIndex[0];
+        
+        await sheet.loadCells({
+            startRowIndex: 1,
+            startColumnIndex: 0,
+            endRowIndex: 20,
+            endColumnIndex: 4
+        });
+        
+        const rows = await sheet.getRows();
+        const data = [];
 
-    async #getData(sheetId, auth) {
-        
+        for (let index = 0; index < rows.length; index++) {
+            const row = rows[index]["_rawData"];
+            if (row.length) {
+                const obj = {};
+                ["companyName", "contactName", "email", "telephone", "website"].forEach((value, index) => {
+                    obj[value] = row[index];
+                });
+                
+                data.push(obj);
+            }
+        }
+
+        return data;
     }
 }
 
